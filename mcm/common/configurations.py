417472902357022de51e31bdbf171f83bd7d6089
alejandroayuso <alejandroayuso@localhost>
2012-07-09 18:09:07 +0200

    pep8 compliance

--- mcm/common/configurations.py
+++ mcm/common/configurations.py
@@ -49,7 +49,7 @@ class McmConfig(object):
     def get_ssh_conf(self):
         conf = self.get_config()
         return (conf['binary-ssh'], conf['default-options-ssh'])
-    
+
     def set_ssh_conf(self, binary, options):
         self.get_config()['binary-ssh'] = binary
         self.get_config()['default-options-ssh'] = options
@@ -62,7 +62,7 @@ class McmConfig(object):
             return (conf['binary-vnc'], conf['default-options-vnc'], embedded)
         except KeyError:
             return (conf['binary-vnc'], conf['default-options-vnc'], False)
-        
+
     def set_vnc_conf(self, binary, options, embedded):
         self.get_config()['binary-vnc'] = binary
         self.get_config()['default-options-vnc'] = options
@@ -71,7 +71,7 @@ class McmConfig(object):
     def get_rdp_conf(self):
         conf = self.get_config()
         return (conf['binary-rdp'], conf['default-options-rdp'])
-    
+
     def set_rdp_conf(self, binary, options):
         self.get_config()['binary-rdp'] = binary
         self.get_config()['default-options-rdp'] = options
@@ -79,7 +79,7 @@ class McmConfig(object):
     def get_telnet_conf(self):
         conf = self.get_config()
         return (conf['binary-telnet'], conf['default-options-telnet'])
-    
+
     def set_telnet_conf(self, binary, options):
         self.get_config()['binary-telnet'] = binary
         self.get_config()['default-options-telnet'] = options
@@ -87,7 +87,7 @@ class McmConfig(object):
     def get_ftp_conf(self):
         conf = self.get_config()
         return (conf['binary-ftp'], conf['default-options-ftp'])
-    
+
     def set_ftp_conf(self, binary, options):
         self.get_config()['binary-ftp'] = binary
         self.get_config()['default-options-ftp'] = options
@@ -105,21 +105,21 @@ class McmConfig(object):
             return conf['color-foreground']
         except KeyError:
             return '#3341ffff0000'
-        
+
     def get_cursor_color(self):
         try:
             conf = self.get_config()
             return conf['color-cursor']
         except KeyError:
             return '#a3d6a3d6a3d6'
-        
+
     def get_selection_color(self):
         try:
             conf = self.get_config()
             return conf['color-selection']
         except KeyError:
             return '#e3d6e3d6e3d6'
-        
+
     def get_tint_color(self):
         try:
             conf = self.get_config()
@@ -129,8 +129,10 @@ class McmConfig(object):
 
     def get_bg_image(self):
         """
-        Checks if the image file given in the configuration exists and is accessible.
-        Returns the path if all conditions are met. Empty string else.
+        Checks if the image file given in the
+        configuration exists and is accessible.
+        Returns the path if all conditions are met.
+        Empty string else.
         """
         conf = self.get_config()
         if os.path.isfile(conf['background-image']) and \
@@ -148,9 +150,10 @@ class McmConfig(object):
 
     def get_bg_saturation(self):
         """
-            If a background image has been set and the saturation value is less than 100, 
-            the terminal will adjust the colors of the image before drawing the image. 
-            To do so, the terminal will create a copy of the background image 
+            If a background image has been set and the saturation
+            value is less than 100, the terminal will adjust the colors
+            of the image before drawing the image.
+            To do so, the terminal will create a copy of the background image
             (or snapshot of the root window) and modify its pixel values.
             Returns a value between 0 and 100
         """
@@ -159,18 +162,18 @@ class McmConfig(object):
             return int(conf['background-saturation']) / 100
         except KeyError:
             return 0
-        
+
     def get_bg_opacity(self):
         """
-            Sets the opacity of the terminal background, were 0 means 
-            completely transparent and 100 means completely opaque. 
+            Sets the opacity of the terminal background, were 0 means
+            completely transparent and 100 means completely opaque.
         """
         try:
             conf = self.get_config()
             return (int(conf['background-opacity']) * 65535) / 100
         except KeyError:
             return 0
-        
+
     def get_pallete(self):
         """
             Returns a list of 14 colors
@@ -182,13 +185,13 @@ class McmConfig(object):
         except Exception:
             print "Failed to get the color palette"
         return None
-        
+
     def get_pallete_name(self):
         try:
             return self.get_config()['color-palette']
         except KeyError:
             return 'DEFAULT'
-    
+
     def set_pallete_name(self, pallete):
         self.get_config()['color-palette'] = pallete
 
@@ -198,7 +201,7 @@ class McmConfig(object):
             return pango.FontDescription(conf['font-name'])
         except KeyError:
             return pango.FontDescription("Monospace 10")
-        
+
     def set_font(self, font):
         self.get_config()['font-name'] = font
 
@@ -208,10 +211,10 @@ class McmConfig(object):
             return conf['word-chars']
         except KeyError:
             return "-A-Za-z0-9,./?&#:_"
-        
+
     def set_word_chars(self, chars):
         self.get_config()['word-chars'] = chars
-        
+
     def get_cursor_shape(self):
         """
             Returns the name of the cursor shape to be used.
@@ -236,51 +239,50 @@ class McmConfig(object):
                 return 500
         except KeyError:
             return 500
-        
+
     def set_buffer_size(self, size):
         if isinstance(size, float):
             size = str(int(size))
         elif isinstance(size, int):
             size = str(size)
         self.get_config()['buffer-size'] = size
-        
+
     # KeyBindings configurations
-    
     def get_kb_tab_switch(self):
         try:
             conf = self.get_config()
             return conf['key-switch']
         except KeyError:
             return "<Alt>"
-    
+
     def get_kb_tab_close(self):
         try:
             conf = self.get_config()
             return conf['key-close']
         except KeyError:
             return "<Shift><Ctrl>w"
-    
+
     def get_kb_copy(self):
         try:
             conf = self.get_config()
             return conf['key-copy']
         except KeyError:
             return "<Shift><Ctrl>c"
-    
+
     def get_kb_paste(self):
         try:
             conf = self.get_config()
             return conf['key-paste']
         except KeyError:
             return "<Shift><Ctrl>v"
-    
+
     def get_kb_home(self):
         try:
             conf = self.get_config()
             return conf['key-home']
         except KeyError:
             return "<Shift><Ctrl>T"
-    
+
     def get_kb_hide(self):
         try:
             conf = self.get_config()
@@ -293,10 +295,10 @@ class McmConfig(object):
             return b
         b = b.strip()
         if len(b) < 1:
-            raise ValueError ('Cannot parse empty string into boolean.')
+            raise ValueError('Cannot parse empty string into boolean.')
         b = b[0].lower()
         if b == 't' or b == 'y' or b == '1':
             return True
         if b == 'f' or b == 'n' or b == '0':
             return False
-        raise ValueError ('Cannot parse string into boolean.')
+        raise ValueError('Cannot parse string into boolean.')
